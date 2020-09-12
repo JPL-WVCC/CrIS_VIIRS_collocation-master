b8b79745caba8e4cf25ca05554118290c646a8a9
leipan <leipan@jpl.nasa.gov>
2020-09-11 20:47:24 -0700

    handled time info in filename

--- code_test_QY.py
+++ code_test_QY.py
@@ -3,11 +3,14 @@ import glob
 import geo_QY
 import time
 import pdb
-import os
+import os, sys
 import pickle
 import netCDF4 as nc4
+from datetime import datetime
 
-start_time = time.time()
+import json
+
+start_t = time.time()
 """
 dataDir1='/peate_archive/.data5/Ops/npp/noaa/op/2012/05/15/scris/'
 dataDir2='/peate_archive/.data5/Ops/npp/noaa/op/2012/05/15/gcrso/'
@@ -15,30 +18,72 @@ dataDir3='/peate_archive/.data5/Ops/npp/noaa/op/2012/05/15/svm15/'
 dataDir4='/peate_archive/.data5/Ops/npp/noaa/op/2012/05/15/gmodo/'
 """
 ### for iday in range(15,23,1):
-for iday in range(15,16,1):
+if True:
 #dataDir2='/peate_archive/.data6/Ops/snpp/gdisc/2/2015/06/01/crisl1b/'
-    dataDir2='/peate_archive/.data1/Ops/snpp/gdisc/2/2015/01/'+str(iday).zfill(2)+'/crisl1b/'
+    ### dataDir2='/peate_archive/.data1/Ops/snpp/gdisc/2/2015/01/'+str(iday).zfill(2)+'/crisl1b/'
+    dataDir2='./'
     ### dataDir4='/raid15/qyue/VIIRS/VIIRS/201501/'
-    dataDir4='/raid15/qyue/VIIRS/VIIRS/201501/VNP03MOD/'
+    ### dataDir4='/raid15/qyue/VIIRS/VIIRS/201501/VNP03MOD/'
+    dataDir4='./'
     
     ### for iloop in range(0,239,10):
-    for iloop in range(0,9,10):
-        print(iloop)   
+    ### for iloop in range(0,9,10):
+    if True:
+        ### print(iloop)   
 # get CrIS files 
 #cris_sdr_files = sorted(glob.glob(dataDir1+'SCRIS*d2012*'))[21:40]
-        cris_geo_files = sorted(glob.glob(dataDir2+'SNDR.SNPP.CRIS*'))[iloop:iloop+10]
+        ### cris_geo_files = sorted(glob.glob(dataDir2+'SNDR.SNPP.CRIS*'))[iloop:iloop+10]
+        cris_geo_files = sorted(glob.glob(dataDir2+'SNDR.SNPP.CRIS*'))
         print ('cris_geo_files: ', cris_geo_files)
 # get VIIRS files 
 #viirs_sdr_files = sorted(glob.glob(dataDir3+'SVM15*d2012*'))[31:59]
-        viirs_geo_files = sorted(glob.glob(dataDir4+'VNP03MOD*A2015'+str(iday).zfill(3)+'*'))[iloop:iloop+10]
+        ### viirs_geo_files = sorted(glob.glob(dataDir4+'VNP03MOD*A2015'+str(iday).zfill(3)+'*'))[iloop:iloop+10]
+        ### viirs_geo_files = sorted(glob.glob(dataDir4+'VNP03MOD*A2015'+'*'))
+        viirs_geo_files = sorted(glob.glob(dataDir4+'VNP03MOD*A*'+'*'))
         print ('viirs_geo_files: ', viirs_geo_files)
+
+
 # read VIIRS data 
-        viirs_lon, viirs_lat, viirs_satAzimuth, viirs_satRange, viirs_satZenith, viirs_height,viirs_time = geo_QY.read_nasa_viirs_geo(viirs_geo_files)
+        viirs_lon, viirs_lat, viirs_satAzimuth, viirs_satRange, viirs_satZenith, viirs_height, viirs_time = geo_QY.read_nasa_viirs_geo(viirs_geo_files)
+        """
+        print ('viirs_time: ', viirs_time)
+        print ('type(viirs_time): ', type(viirs_time))
+        print ('viirs_time.min(): ', viirs_time.min())
+        print ('viirs_time.max(): ', viirs_time.max())
+        """
+        start_time = viirs_time.min()
+        end_time = viirs_time.max()
+
+
 #viirs_bt, viirs_rad, viirs_sdrQa = geo.read_viirs_sdr(viirs_sdr_files)i
 
 
 # read CrIS data 
         cris_lon, cris_lat, cris_satAzimuth, cris_satRange, cris_satZenith, cris_time, cris_realLW = geo_QY.read_nasa_cris_geo(cris_geo_files)
+        """
+        print ('cris_time: ', cris_time)
+        print ('cris_time.min(): ', cris_time.min())
+        print ('cris_time.max(): ', cris_time.max())
+        """
+        if start_time < cris_time.min():
+          start_time = cris_time.min()
+
+        if end_time > cris_time.max():
+          end_time = cris_time.max()
+
+        print ('start_time: ', start_time)
+        print ('end_time: ', end_time)
+
+        start_date = time.strftime('%Y%m%dT%H%M%S', time.localtime(start_time))
+        end_date = time.strftime('%Y%m%dT%H%M%S', time.localtime(end_time))
+
+        print ('start_date: ', start_date)
+        print ('end_date: ', end_date)
+
+        output_filename = 'IND_CrIS_VIIRSMOD_' + start_date + '_' + end_date
+        print ('output_filename: ', output_filename)
+
+        ### sys.exit(0)
 
 #cris_realLW = geo.read_nasa_cris_sdr(cris_sdr_files , sdrFlag=True)
 
@@ -76,7 +121,9 @@ for iday in range(15,16,1):
         dx_flatten = np.array([item for lst in dx.reshape(-1) for item in lst])
 
         ### f = nc4.Dataset('/raid15/qyue/VIIRS/VIIRS/201501/Index/IND_CrIS_VIIRSMOD_201501'+str(iday)+'_'+str(iloop)+'.nc','w', format='NETCDF4') #'w' stands for write
-        f = nc4.Dataset('/raid15/leipan/VIIRS/VIIRS/201501/Index/IND_CrIS_VIIRSMOD_201501'+str(iday)+'_'+str(iloop)+'.nc','w', format='NETCDF4') #'w' stands for write
+        ### f = nc4.Dataset('/raid15/leipan/VIIRS/VIIRS/201501/Index/IND_CrIS_VIIRSMOD_201501'+str(iday)+'_'+str(iloop)+'.nc','w', format='NETCDF4') #'w' stands for write
+        ### f = nc4.Dataset('./IND_CrIS_VIIRSMOD_201501'+'.nc','w', format='NETCDF4') #'w' stands for write
+        f = nc4.Dataset(output_filename+'.nc','w', format='NETCDF4') #'w' stands for write
 
         f.createDimension('m',dy_flatten.size)
         f.createDimension('x', dy.shape[0])
@@ -96,10 +143,46 @@ for iday in range(15,16,1):
         f.description="Demo Data for 2015 Jan"
 
         f.close()
-        
-    print("done in --- %s seconds --- for 2015 Jan %d files" % (time.time() - start_time, iday))
-    start_time=time.time()
+
+
+# datetime object containing current date and time
+now = datetime.now()
+print("now: ", now)
+
+# dd/mm/YY H:M:S
+### dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+dt_string = now.strftime("%Y%m%dT%H%M%S")
+print("date and time =", dt_string)	
+
+d1 = \
+{
+    "creation_timestamp": dt_string,
+    "version": "v1.0",
+    "endtime": start_date+"Z",
+    "starttime": end_date+"Z",
+    "label": "matchup_cris_viirs_"+ start_date + '_' + end_date
+}
+with open(output_filename+'.dataset.json', 'w') as datasetf:
+    json.dump(d1, datasetf)
+
+d2 = {}
+with open(output_filename+'.met.json', 'w') as metf:
+    json.dump(d2, metf)
+
+
+print("done in --- %.2f seconds --- " % (float(time.time() - start_t)))
+
 # collocation is done
+
+
+
+
+
+
+
+
+
+
 """
 ##############################################################################
 # showing the collocated images 
